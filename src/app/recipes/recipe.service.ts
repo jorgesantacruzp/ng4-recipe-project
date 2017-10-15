28b7842e3b2d0939f2cb2b48ab36306fa8e45e87
Jorge Santa Cruz <jorge.santacruzp@gmail.com>
2017-10-15 18:41:07 -0500

    Ingredients were added to each recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import {Recipe} from "./recipe.model";
 import {EventEmitter} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
 
 export class RecipeService {
 
@@ -8,10 +9,18 @@ export class RecipeService {
   private recipes: Recipe[] = [
     new Recipe('Estofado',
       'Best dish ever.',
-      'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg')
-    ,new Recipe('Burger',
+      'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg',
+      [
+        new Ingredient('Chicken', 1),
+        new Ingredient('Potato', 2)
+      ])
+    , new Recipe('Burger',
       'This is a big burger.',
-      'http://bk-ca-prd.s3.amazonaws.com/sites/burgerking.ca/files/Hero-Cheeseburger.png')
+      'http://bk-ca-prd.s3.amazonaws.com/sites/burgerking.ca/files/Hero-Cheeseburger.png',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Bread', 1)
+      ])
   ];
 
   getRecipes() {
