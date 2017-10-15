da81f98b7a432f9074f397a12363bb7130940735
Jorge Santa Cruz <jorge.santacruzp@gmail.com>
2017-10-15 18:26:12 -0500

    Recipes were modified

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -6,9 +6,12 @@ export class RecipeService {
   selectedRecipe = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe','This is simply a test.','http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg')
-    ,new Recipe('A Test Recipe TWO','This is simply a test TWO.','http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg')
-    ,new Recipe('A Test Recipe THREE','This is simply a test THREE.','http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg')
+    new Recipe('Estofado',
+      'Best dish ever.',
+      'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg')
+    ,new Recipe('Burger',
+      'This is a big burger.',
+      'http://bk-ca-prd.s3.amazonaws.com/sites/burgerking.ca/files/Hero-Cheeseburger.png')
   ];
 
   getRecipes() {
